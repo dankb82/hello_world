982b217c815145ce616e21c2a5a16f395257640c
Daniel Buompane <danielbuompane@gmail.com>
2017-05-07 18:48:30 -0400

    Add firstName variable

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String firstName = "Daniel";
+        System.out.println("Hello " + firstName);
     }
 }
